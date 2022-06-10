9e9db236e55ac2fa09416d2c2a63cad7969a6ba3
MyMiDiII <mymidi.student@yandex.ru>
2022-06-10 22:29:27 +0300

    ADD: first VenueService method

---
+++ src/BusinessLogic/Services/VenueService.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLogic.Models;
+using BusinessLogic.IRepositories;
+
+namespace BusinessLogic.Services
+{
+    public class VenueService : IVenueService
+    {
+        private IVenueRepository _venueRepository;
+
+        public VenueService(IVenueRepository venueRepository)
+        {
+            _venueRepository = venueRepository;
+        }
+
+        public List<Venue> GetVenues()
+        {
+            return _venueRepository.GetAll();
+        }
+    }
+
+    public interface IVenueService
+    {
+        List<Venue> GetVenues();
+    }
+}

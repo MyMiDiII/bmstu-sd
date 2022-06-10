67365e082d1bc45153fa1682ad57dc6da35e45ea
MyMiDiII <mymidi.student@yandex.ru>
2022-06-10 14:44:17 +0300

    FIX: extra using and date/time type

--- src/BusinessLogic/Models/BoardGameEvent.cs
+++ src/BusinessLogic/Models/BoardGameEvent.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BusinessLogic.Models
+﻿namespace BusinessLogic.Models
 {
     public class BoardGameEvent
     {
         public long ID { get; set; }
         public string Title { get; set; }
-        public DateTime Date { get; set; }
-        public DateTime StartTime { get; set; }
+        public DateOnly Date { get; set; }
+        public TimeOnly StartTime { get; set; }
         public uint Duration { get; set; }
         public uint Cost { get; set; }
         public bool Purchase { get; set; }
-        public DateTime RegistrationTime { get; set; }
+        public TimeOnly RegistrationTime { get; set; }
     }
 }

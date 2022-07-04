d9e0470000e07837056bae38be059ba881806d94
MyMiDiII <mymidi.student@yandex.ru>
2022-07-04 15:45:38 +0300

    ADD: organizer deleted field and organizer repository exceptions

--- src/DataAccess/Repositories/OrganizerRepository.cs
+++ src/DataAccess/Repositories/OrganizerRepository.cs
@@ -2,6 +2,7 @@
 
 using BusinessLogic.Models;
 using BusinessLogic.IRepositories;
+using BusinessLogic.Exceptions;
 
 namespace DataAccess.Repositories
 {
@@ -16,13 +17,20 @@ namespace DataAccess.Repositories
 
         public void Add(Organizer elem)
         {
-            _dbcontext.Organizers.Add(elem);
-            _dbcontext.SaveChanges();
+            try
+            {
+                _dbcontext.Organizers.Add(elem);
+                _dbcontext.SaveChanges();
+            }
+            catch
+            {
+                throw new AddOrganizerException();
+            }
         }
 
         public List<Organizer> GetAll()
         {
-            return _dbcontext.Organizers.ToList();
+            return _dbcontext.Organizers.Where(organizer => !organizer.Deleted).ToList();
         }
 
         public Organizer? GetByID(long id)
@@ -32,28 +40,44 @@ namespace DataAccess.Repositories
 
         public void Update(Organizer elem)
         {
-            _dbcontext.Organizers.Update(elem);
-            _dbcontext.SaveChanges();
-        }
+            try
+            {
+                _dbcontext.Organizers.Update(elem);
+                _dbcontext.SaveChanges();
+            }
+            catch
+            {
+                throw new UpdateOrganizerException();
+            }
+       }
 
         public void Delete(Organizer elem)
         {
+            var tmp = _dbcontext.Organizers.Find(elem.ID);
+
+            if (tmp is null)
+                throw new NotExistsOrganizerException();
+
+            if (tmp.Deleted)
+                throw new AlreadyDeletedOrganizerException();
+
+            tmp.Deleted = true;
+            Update(tmp);
         }
 
         public List<Organizer> GetByName(string name)
         {
             return _dbcontext.Organizers
-                   .Where(organizer => organizer.Name.Contains(name, StringComparison.OrdinalIgnoreCase))
+                   .Where(organizer => !organizer.Deleted && organizer.Name.Contains(name, StringComparison.OrdinalIgnoreCase))
                    .ToList();
         }
 
         public List<Organizer> GetByAddress(string address)
         {
             return _dbcontext.Organizers
-                   .Where(organizer => organizer.Address.Contains(address, StringComparison.OrdinalIgnoreCase))
+                   .Where(organizer => !organizer.Deleted && organizer.Address.Contains(address, StringComparison.OrdinalIgnoreCase))
                    .ToList();
         }
-        
         public List<BoardGameEvent> GetOrganizerEvents(long organizerID)
         {
             return _dbcontext.Events

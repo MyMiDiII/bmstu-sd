5ee99e3e5a698e0b9469babc6bbbf7df77245ca2
MyMiDiII <mymidi.student@yandex.ru>
2022-07-08 10:46:22 +0300

    FIX: repos interfaces by entities relations (lab03)

--- src/BusinessLogic/IRepositories/IBoardGameRepository.cs
+++ src/BusinessLogic/IRepositories/IBoardGameRepository.cs
@@ -10,6 +10,12 @@ namespace BusinessLogic.IRepositories
         List<BoardGame> GetByAge(uint minAge, uint maxAge);
         List<BoardGame> GetByPlayerNum(uint minNum, uint maxNum);
         List<BoardGame> GetByDuration(uint minDuration, uint maxDuration);
+        void AddToEvent(long gameID, long eventID);
+        void DeleteFromEvent(long gameID, long eventID);
+        bool CheckGamePlaying(long gameID, long eventID);
         List<BoardGameEvent> GetGameEvents(long gameID);
+        void AddToFavorites(long gameID, long playerID);
+        void DeleteFromFavorites(long gameID, long playerID);
+        bool CheckGameInFavorites(long gameID, long playerID);
     }
 }

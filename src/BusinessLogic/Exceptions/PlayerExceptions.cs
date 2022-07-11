74c5e4f9fadebe2a5581c7c23893fbbad0cf71e5
MyMiDiII <mymidi.student@yandex.ru>
2022-07-11 15:48:34 +0300

    ADD: player repo implementation

--- src/BusinessLogic/Exceptions/PlayerExceptions.cs
+++ src/BusinessLogic/Exceptions/PlayerExceptions.cs
@@ -12,4 +12,7 @@
     public class UserIsNotPlayerException : PlayerException { }
     public class AlreadyExistsPlayerRegistraionException : PlayerException { }
     public class NotExistsPlayerRegistraionException : PlayerException { }
+    public class AddPlayerException : PlayerException { }
+    public class UpdatePlayerException : PlayerException { }
+    public class AlreadyDeletedPlayerException : PlayerException { }
 }

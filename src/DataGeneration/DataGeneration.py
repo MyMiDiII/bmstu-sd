a3596c4bdab0e17e7c8b3f358857c1d017e5784d
MyMiDiII <mymidi.student@yandex.ru>
2022-08-28 17:25:34 +0300

    ADD: organizers and venues data generation

--- src/DataGeneration/DataGeneration.py
+++ src/DataGeneration/DataGeneration.py
@@ -1,7 +1,11 @@
 import BoardGamesGen as BGG
+import OrganizersGen as OG
+import VenuesGen as VG
 
 def generate_data():
-    BGG.generate_boardgames();
+    #BGG.generate_boardgames();
+    OG.generate_organizers("ru");
+    VG.generate_venues("ru");
 
 if __name__ == "__main__":
     generate_data();
